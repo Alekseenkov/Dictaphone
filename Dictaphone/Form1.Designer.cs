b3f5ccf97b6dab000d2362b8754edfe62db8365a
Aleksandr <kseenkovale@gmail.com>
2021-05-25 23:54:21 +0300

    Добавьте файлы проекта.

---
+++ Dictaphone/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿
+namespace Dictaphone
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonPlay = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.labelTimeRecord = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonPlay
+            // 
+            this.buttonPlay.Location = new System.Drawing.Point(13, 92);
+            this.buttonPlay.Name = "buttonPlay";
+            this.buttonPlay.Size = new System.Drawing.Size(94, 29);
+            this.buttonPlay.TabIndex = 2;
+            this.buttonPlay.Text = "Play";
+            this.buttonPlay.UseVisualStyleBackColor = true;
+            this.buttonPlay.Click += new System.EventHandler(this.buttonPlay_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(248, 169);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 29);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Pause";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.buttonPause_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(348, 169);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 29);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Record";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.buttonRecord_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // labelTimeRecord
+            // 
+            this.labelTimeRecord.AutoSize = true;
+            this.labelTimeRecord.Location = new System.Drawing.Point(329, 125);
+            this.labelTimeRecord.Name = "labelTimeRecord";
+            this.labelTimeRecord.Size = new System.Drawing.Size(30, 20);
+            this.labelTimeRecord.TabIndex = 5;
+            this.labelTimeRecord.Text = ".......";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(3, 46);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(232, 28);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "label1";
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(176, 92);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(59, 29);
+            this.buttonDelete.TabIndex = 8;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(476, 237);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.labelTimeRecord);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.buttonPlay);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonPlay;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label labelTimeRecord;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonDelete;
+    }
+}
+
---
+++ Dictaphone/Form1.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NAudio;
+using NAudio.Wave;
+using NAudio.FileFormats;
+using NAudio.CoreAudioApi;
+using System.IO;
+
+namespace Dictaphone
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// ////////////колхоз переделать 
+        /// </summary>
+        // WaveIn - поток для записи
+        WaveIn waveIn;
+        //Класс для записи в файл
+        WaveFileWriter writer;
+        //Имя файла для записи
+        //string outputFilename = @"D:\DistaphoneMedia\1.wav";
+
+
+        //////////////
+        ///
+        List<string> Filenames = new List<string>();
+
+
+
+
+        /// <summary>
+        /// //////////
+        /// </summary>
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        void updateFileList()
+        {
+            ////////говнокод 
+
+            for (int i = 0; i <10; i++)
+            {
+                Filenames.Add("");
+            }
+
+            comboBox1.Items.Clear();
+            var dir = new DirectoryInfo(@"D:\DistaphoneMedia\");// папка с файлами 
+            // var files = new List<string>(); // список для имен файлов 
+
+            foreach (FileInfo file in dir.GetFiles()) // извлекаем все файлы и кидаем их в список 
+            {
+                string numfile = file.Name.Substring(0, 1);
+
+                // Filenames.Add(file.Name);//имя файла 
+                Filenames[Convert.ToInt32(numfile)] = file.FullName;// получаем полный путь к файлу и кидаем его в список 
+                comboBox1.Items.Add(file.Name);
+                //comboBox1.Items.Add(file.Name.Substring(0, 1));
+            }
+
+        }
+
+
+
+        //Получение данных из входного буфера 
+        void waveIn_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            if (this.InvokeRequired)
+            {
+                this.BeginInvoke(new EventHandler<WaveInEventArgs>(waveIn_DataAvailable), sender, e);
+            }
+            else
+            {
+                //Записываем данные из буфера в файл
+                writer.WriteData(e.Buffer, 0, e.BytesRecorded);
+            }
+        }
+        //Завершаем запись
+        void StopRecording()
+        {
+            // MessageBox.Show("StopRecording");
+            timer1.Enabled = false;
+            labelTimeRecord.Text = ".....";
+            recordTime = 0;
+            //////////////
+            waveIn.StopRecording();
+            ///обновление комбо бокса
+            updateFileList();
+            comboBox1.Text = "";
+        }
+        //Окончание записи
+        private void waveIn_RecordingStopped(object sender, EventArgs e)
+        {
+            if (this.InvokeRequired)
+            {
+                this.BeginInvoke(new EventHandler(waveIn_RecordingStopped), sender, e);
+            }
+            else
+            {
+                waveIn.Dispose();
+                waveIn = null;
+                writer.Close();
+                writer = null;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            updateFileList();
+
+        }
+
+        private void buttonNo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonYes_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonPlay_Click(object sender, EventArgs e)
+        {
+            //////
+            WaveStream mainOutputStream = new WaveFileReader(@"D:\DistaphoneMedia\" + comboBox1.SelectedItem.ToString());
+            WaveChannel32 volumeStream = new WaveChannel32(mainOutputStream);
+
+            WaveOutEvent player = new WaveOutEvent();
+
+            player.Init(volumeStream);
+            player.Play();
+
+        }
+
+        private void buttonPause_Click(object sender, EventArgs e)
+        {
+            if (waveIn != null)
+            {
+                StopRecording();
+            }
+
+        }
+
+        private void buttonRecord_Click(object sender, EventArgs e)
+        {//////
+              //треш в том что при загрузке в листе файлов
+               // нет пропусков на месте файлов кроторых нет 
+
+           // пиздец это нужно как нибудь решать 
+
+
+            //проверка списка файлов 
+            if (Filenames.IndexOf("") == -1)
+            {
+                MessageBox.Show("Места нет Удалите записи");
+            }
+            else {
+                int findex = Filenames.IndexOf("");
+                Filenames[findex]= @"D:\DistaphoneMedia\" + findex.ToString() + ".wav";
+
+            //}
+
+            //if (Filenames.Count == 10)
+            //{
+            //    MessageBox.Show("места нет");
+            //}
+            //else
+           // {
+                Filenames.Add(@"D:\DistaphoneMedia\" + Filenames.Count.ToString() + ".wav");
+
+
+                ////////// старт таймера на запись 
+                timer1.Enabled = true;
+
+                ////////////
+
+                try
+                {
+                    //  MessageBox.Show("Start Recording");
+                    waveIn = new WaveIn();
+                    //Дефолтное устройство для записи (если оно имеется)
+                    waveIn.DeviceNumber = 0;
+                    //Прикрепляем к событию DataAvailable обработчик, возникающий при наличии записываемых данных
+                    waveIn.DataAvailable += waveIn_DataAvailable;
+                    //Прикрепляем обработчик завершения записи
+                    waveIn.RecordingStopped += waveIn_RecordingStopped;
+
+                    // waveIn.RecordingStopped += new EventHandler(waveIn_RecordingStopped);
+
+                    //Формат wav-файла - принимает параметры - частоту дискретизации и количество каналов(здесь mono)
+                    waveIn.WaveFormat = new WaveFormat(8000, 1);
+                    //Инициализируем объект WaveFileWriter
+                    //writer = new WaveFileWriter(outputFilename, waveIn.WaveFormat);
+                    writer = new WaveFileWriter(Filenames[findex], waveIn.WaveFormat);
+                    //Начало записи
+                    waveIn.StartRecording();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+
+        }
+
+        int recordTime = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            recordTime++;
+            /////////////////////
+            if (recordTime > 10) {
+                if (waveIn != null)
+                {
+                    StopRecording();
+                }
+
+
+            }
+
+            ///////////////////////////////
+            labelTimeRecord.Text = recordTime.ToString();
+
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+           
+            //////удаление файла из списка 
+            Filenames[Filenames.IndexOf(@"D:\DistaphoneMedia\" + comboBox1.SelectedItem.ToString())] = "";
+            /////удаление файлов из папки 
+            File.Delete(@"D:\DistaphoneMedia\" + comboBox1.SelectedItem.ToString());
+
+            ///обновление комбо бокса
+            updateFileList();
+            comboBox1.Text = "";
+
+
+        }
+    }
+}
+
